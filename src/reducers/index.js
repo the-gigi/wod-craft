14997ce87bee2c77f218f04163a99bc925ed6d9e
Gigi <gigi.sayfan@aclimalabs.com>
2016-05-22 11:40:47 -0700

    Added reducers to fetch scores from the server

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,37 @@
  *          This modifications only run once when the generator is invoked - if
  *          you edit them, they are not updated again.
  */
-import { combineReducers } from 'redux';
-/* Populated by react-webpack-redux:reducer */
-const reducers = {};
-module.exports = combineReducers(reducers);
+
+
+import { combineReducers } from 'redux'
+import {
+    REQUEST_SCORES,
+    RECEIVE_SCORES
+} from '../actions/actions'
+
+
+function scores(state = {
+    isFetching: false,
+    items: []
+}, action) {
+    switch (action.type) {
+        case REQUEST_SCORES:
+            return Object.assign({}, state, {
+                isFetching: true
+            });
+        case RECEIVE_SCORES:
+            console.log('Yay! received scores');
+            return Object.assign({}, state, {
+                isFetching: false,
+                items: action.scores
+            });
+        default:
+            return state
+    }
+}
+
+const rootReducer = combineReducers({
+    scores
+});
+
+export default rootReducer
